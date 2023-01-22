6d9a027ebe30a354b4cd9780446bb81d7faff2bc
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2023-01-22 15:24:46 +0530

    refactor: test instatus

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
-const STATUS_PAGE_URL = "https://stats.uptimerobot.com/pzxzYf90DR/788159955";
+// const STATUS_PAGE_URL = "https://stats.uptimerobot.com/pzxzYf90DR/788159955";
+const STATUS_PAGE_URL = "https://abhijithvijayan.instatus.com";
 const SERVER_URL = "https://site-fetcher.deta.dev/api/v1/html";
 
 const events = {
